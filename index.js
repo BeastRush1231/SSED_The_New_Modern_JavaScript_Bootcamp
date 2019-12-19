77eb83193af8086eea86ee48cf234040060d73b5
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-19 16:11:20 +0800

    1219 MSP-Section20 Drawing Animation First Pass on the Animation

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI; //取得圓周率
+circle.setAttribute('stroke-dasharray', perimeter); //設定外框圈圈
+
+let currentOffset = 0;
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
     console.log('Timer started');
   },
   onTick() {
-    console.log('Timer just ticked down');  
+    circle.setAttribute('stroke-dashoffset', currentOffset) //設定外框距離
+    currentOffset = currentOffset - 50;	//每次減少offset距離
   },
   onComplete() {
     console.log('Timer is completed');
