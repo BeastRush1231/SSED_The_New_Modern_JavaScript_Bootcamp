bed14638c38b590e6f8392572a173f12c17ef309
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-21 13:52:27 +0800

    1221 MSP-Section20 Movie Fetching a Single Movie

--- Movie/index.js
+++ Movie/index.js
@@ -2,7 +2,7 @@ const fetchData = async () => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '33e086f5',
-      s: 'avengers'
+      i: "tt0848228"
     }
   });
   
