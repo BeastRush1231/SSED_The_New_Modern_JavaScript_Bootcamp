fedab6c65ca3e77cc81456d6869f479392f95ae2
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-23 22:24:39 +0800

    1223 MSP-Section21 Movie Small Bug Fix

--- Movie/index.js
+++ Movie/index.js
@@ -75,8 +75,8 @@ const runComparsion = () => {
   leftSideStats.forEach( (leftStat, index) => {
     const rightStat = rightSideStats[index];
     // console.log(leftStat, rightStat);
-    const leftSideValue = leftStat.dataset.value;
-    const rightSideValue = rightStat.dataset.value;
+    const leftSideValue = parseInt(leftStat.dataset.value);
+    const rightSideValue = parseInt(rightStat.dataset.value);
 
     if (rightSideValue > leftSideValue){
       leftStat.classList.remove('is-primary');
