c383d1d0b2dfb0821a7bd7944b069d8835a8c5df
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-26 21:07:42 +0800

    1226 MSP-Section22 CanvasAPI Generating Random Shapes

--- Canvas API/index.js
+++ Canvas API/index.js
@@ -1,13 +1,17 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
+const width = 800;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes: false, //顏色
+    width, // k = v
+    height
   }
 });
 
@@ -27,4 +31,19 @@ const walls = [
   Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
 ];
 World.add(world, walls);
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+
+// Render Shapes
+
+for (let i = 0; i < 50; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world, 
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    );
+  } else {
+    World.add(
+      world, 
+      Bodies.circle(Math.random() * width, Math.random() * height, 35)
+    );
+  }
+}
