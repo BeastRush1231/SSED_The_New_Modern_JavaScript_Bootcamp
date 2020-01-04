290de4a8e04e124f3db6157c7d898ca201d93f3f
BeastRush1231 <M0761005@g2.usc.edu.tw>
2020-01-05 00:32:51 +0800

    0104 MSP-Section23 Message Generating the URL

--- Message/index.js
+++ Message/index.js
@@ -1,8 +1,13 @@
 document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault();
 
-  const input = document.querySelector('#message-input');
-  const encrypted = btoa(input.value);
+  const messageInput = document.querySelector('#message-input');
+  const encrypted = btoa(messageInput.value);
 
-  document.querySelector('#link-input').value = encrypted;
+  // document.querySelector('#link-input').value = `${window.location}#${encrypted}`;
+
+  const linkInput = document.querySelector('#link-input');
+  linkInput.value = `${window.location}#${encrypted}`;
+  // 使用者可以複製 HTMLInputElement.select()
+  linkInput.select();
 });
