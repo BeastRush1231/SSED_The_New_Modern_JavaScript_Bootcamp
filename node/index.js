a2ddb3c9829a9fc0ed4adae3164b78caeb044520
BeastRush1231 <M0761005@g2.usc.edu.tw>
2020-01-06 21:42:28 +0800

    0106 MSP-Section24 Node File Get Required Once

--- node/index.js
+++ node/index.js
@@ -1,51 +1,9 @@
-require('./myscript.js');
+const counterObject = require('./myscript.js');
 
-console.log(require.cache);
+console.log(counterObject.getCounter()); // 0
+counterObject.incrementCounter();
+console.log(counterObject.getCounter()); // 1
 
-//[Object: null prototype] {
-//   '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/index.js': Module {
-//     id: '.',
-//     path: '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node',
-//     exports: {},
-//     parent: null,
-//     filename: '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/index.js',
-//     loaded: false,
-//     children: [ [Module] ],
-//     paths: [
-//       '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/node_modules',
-//       '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node_modules',
-//       '/Users/chester/Desktop/learning/node_modules',
-//       '/Users/chester/Desktop/node_modules',
-//       '/Users/chester/node_modules',
-//       '/Users/node_modules',
-//       '/node_modules'
-//     ]
-//   },
-//   '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/myscript.js': Module {
-//     id: '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/myscript.js',
-//     path: '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node',
-//     exports: 'hi there',
-//     parent: Module {
-//       id: '.',
-//       path: '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node',
-//       exports: {},
-//       parent: null,
-//       filename: '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/index.js',
-//       loaded: false,
-//       children: [Array],
-//       paths: [Array]
-//     },
-//     filename: '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/myscript.js',
-//     loaded: true,
-//     children: [],
-//     paths: [
-//       '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/node_modules',
-//       '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node_modules',
-//       '/Users/chester/Desktop/learning/node_modules',
-//       '/Users/chester/Desktop/node_modules',
-//       '/Users/chester/node_modules',
-//       '/Users/node_modules',
-//       '/node_modules'
-//     ]
-//   }
-// }
+const newcounterObject = require('./myscript.js');
+
+console.log(newcounterObject.getCounter()); // 1
