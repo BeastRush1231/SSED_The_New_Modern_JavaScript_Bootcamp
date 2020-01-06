4260742c9a21f8094a57a3f3dbf0ba58f0704682
BeastRush1231 <M0761005@g2.usc.edu.tw>
2020-01-06 21:16:18 +0800

    0106 MSP-Section24 Node Invisible Node Functions

--- node/index.js
+++ node/index.js
@@ -1,3 +1,69 @@
-const message = require('./myscript.js')
+console.log(exports);
+// {}
 
-console.log(message);
+console.log(require);
+// [Function: require] {
+//   resolve: [Function: resolve] { paths: [Function: paths] },
+//   main: Module {
+//     id: '.',
+//     path: '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node',
+//     exports: {},
+//     parent: null,
+//     filename: '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/index.js',
+//     loaded: false,
+//     children: [],
+//     paths: [
+//       '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/node_modules',
+//       '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node_modules',
+//       '/Users/chester/Desktop/learning/node_modules',
+//       '/Users/chester/Desktop/node_modules',
+//       '/Users/chester/node_modules',
+//       '/Users/node_modules',
+//       '/node_modules'
+//     ]
+//   },
+//   extensions: [Object: null prototype] {
+//     '.js': [Function],
+//     '.json': [Function],
+//     '.node': [Function],
+//     '.mjs': [Function]
+//   },
+//   cache: [Object: null prototype] {
+//     '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/index.js': Module {
+//       id: '.',
+//       path: '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node',
+//       exports: {},
+//       parent: null,
+//       filename: '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/index.js',
+//       loaded: false,
+//       children: [],
+//       paths: [Array]
+//     }
+//   }
+// }
+
+console.log(module);
+// Module {
+//   id: '.',
+//   path: '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node',
+//   exports: {},
+//   parent: null,
+//   filename: '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/index.js',
+//   loaded: false,
+//   children: [],
+//   paths: [
+//     '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/node_modules',
+//     '/Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node_modules',
+//     '/Users/chester/Desktop/learning/node_modules',
+//     '/Users/chester/Desktop/node_modules',
+//     '/Users/chester/node_modules',
+//     '/Users/node_modules',
+//     '/node_modules'
+//   ]
+// }
+
+console.log(__filename);
+// /Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node/index.js
+
+console.log(__dirname);
+// /Users/chester/Desktop/learning/SSED_The_New_Modern_JavaScript_Bootcamp/node
